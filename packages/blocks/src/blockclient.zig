de1d021b928f7d556b5817af50768d0db7ce9b0d
pfg <pfg@pfg.pw>
2024-08-20 23:58:28 -0700

    fmt

--- packages/blocks/src/blockclient.zig
+++ packages/blocks/src/blockclient.zig
@@ -5,13 +5,13 @@ pub fn main() !void {
     defer std.debug.assert(gpa_alloc.deinit() == .ok);
     const gpa = gpa_alloc.allocator();
 
-    const client = try std.net.tcpConnectToAddress(std.net.Address.initIp4(.{127, 0, 0, 1}, 3667));
+    const client = try std.net.tcpConnectToAddress(std.net.Address.initIp4(.{ 127, 0, 0, 1 }, 3667));
 
     const client_reader = client.reader();
     const client_writer = client.writer();
 
     try client_writer.writeAll("ping!\n");
-    
+
     _ = client_reader;
     _ = gpa;
 }
--- packages/blocks/src/blockserver.zig
+++ packages/blocks/src/blockserver.zig
@@ -5,17 +5,17 @@ pub fn main() !void {
     defer std.debug.assert(gpa_alloc.deinit() == .ok);
     const gpa = gpa_alloc.allocator();
 
-    const addr = std.net.Address.initIp4(.{0, 0, 0, 0}, 3667);
+    const addr = std.net.Address.initIp4(.{ 0, 0, 0, 0 }, 3667);
     var server = try addr.listen(.{});
 
     std.log.info("Server listening on port 3667", .{});
-    
+
     var client = try server.accept();
     defer client.stream.close();
 
     const client_reader = client.stream.reader();
     const client_writer = client.stream.writer();
-    while(true) {
+    while (true) {
         const msg = try client_reader.readUntilDelimiterOrEofAlloc(gpa, '\n', 65536) orelse break;
         defer gpa.free(msg);
 
