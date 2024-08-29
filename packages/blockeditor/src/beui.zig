5b546ea8070d247ed8913a4114e5fdaf7a397fa3
pfg <pfg@pfg.pw>
2024-08-29 09:27:13 -0700

    .

--- packages/blockeditor/src/beui.zig
+++ packages/blockeditor/src/beui.zig
@@ -16,38 +16,36 @@ const zstbi = @import("zstbi");
 const content_dir = @import("build_options").content_dir;
 const window_title = "zig-gamedev: textured quad (wgpu)";
 
-// zig fmt: off
-const wgsl_common =
-\\  struct Uniforms {
-\\      aspect_ratio: f32,
-\\      mip_level: f32,
-\\  }
-\\  @group(0) @binding(0) var<uniform> uniforms: Uniforms;
-\\
-\\  struct VertexOut {
-\\      @builtin(position) position_clip: vec4<f32>,
-\\      @location(0) uv: vec2<f32>,
-\\  }
-\\  @vertex fn vert(
-\\      @location(0) position: vec2<f32>,
-\\      @location(1) uv: vec2<f32>,
-\\  ) -> VertexOut {
-\\      let p = vec2(position.x / uniforms.aspect_ratio, position.y);
-\\      var output: VertexOut;
-\\      output.position_clip = vec4(p, 0.0, 1.0);
-\\      output.uv = uv;
-\\      return output;
-\\  }
-\\
-\\  @group(0) @binding(1) var image: texture_2d<f32>;
-\\  @group(0) @binding(2) var image_sampler: sampler;
-\\  @fragment fn frag(
-\\      @location(0) uv: vec2<f32>,
-\\  ) -> @location(0) vec4<f32> {
-\\      return textureSampleLevel(image, image_sampler, uv, uniforms.mip_level);
-\\  }
-// zig fmt: on
-;
+const wgsl_common = (
+    \\  struct Uniforms {
+    \\      aspect_ratio: f32,
+    \\      mip_level: f32,
+    \\  }
+    \\  @group(0) @binding(0) var<uniform> uniforms: Uniforms;
+    \\
+    \\  struct VertexOut {
+    \\      @builtin(position) position_clip: vec4<f32>,
+    \\      @location(0) uv: vec2<f32>,
+    \\  }
+    \\  @vertex fn vert(
+    \\      @location(0) position: vec2<f32>,
+    \\      @location(1) uv: vec2<f32>,
+    \\  ) -> VertexOut {
+    \\      let p = vec2(position.x / uniforms.aspect_ratio, position.y);
+    \\      var output: VertexOut;
+    \\      output.position_clip = vec4(p, 0.0, 1.0);
+    \\      output.uv = uv;
+    \\      return output;
+    \\  }
+    \\
+    \\  @group(0) @binding(1) var image: texture_2d<f32>;
+    \\  @group(0) @binding(2) var image_sampler: sampler;
+    \\  @fragment fn frag(
+    \\      @location(0) uv: vec2<f32>,
+    \\  ) -> @location(0) vec4<f32> {
+    \\      return textureSampleLevel(image, image_sampler, uv, uniforms.mip_level);
+    \\  }
+);
 
 const Vertex = extern struct {
     position: [2]f32,
