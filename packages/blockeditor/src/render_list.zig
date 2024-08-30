da93cbdd91d91b8136665b9ff90eaf8c6cf7cbcb
pfg <pfg@pfg.pw>
2024-08-29 21:54:22 -0700

    new method that just needs one vertex buffer and index buffer to be posted

--- packages/blockeditor/src/render_list.zig
+++ packages/blockeditor/src/render_list.zig
@@ -8,42 +8,60 @@ const std = @import("std");
 // - that way we copy one vertex array and then draw repeatedly from
 //   different parts of the vertex array
 
+// we could, if everything is a rectangle, just have 6 vertices
+// and do everything with instanced draw calls
+// probably saves on some bytes, who knows if it's faster though.
+
 pub const RenderListImage = enum(u64) { _ };
-pub const RenderListIndex = u16;
+pub const RenderListIndex = u32;
 pub const RenderListVertex = struct {
     pos: @Vector(2, f32),
     uv: @Vector(2, f32),
     tint: @Vector(4, f32),
     // TODO: rounding, shadow
 };
+pub const RenderListCommand = struct {
+    vertex_count: usize, // internal only, wgpu doesn't care
+    index_count: u32,
+    first_index: u32,
+    base_vertex: i32, // not sure if this is what I think it is
+    image: ?RenderListImage,
+};
 
 pub const RenderList = struct {
     gpa: std.mem.Allocator,
-    sub_lists: std.ArrayList(RenderListSegment),
+    vertices: std.ArrayList(RenderListVertex),
+    indices: std.ArrayList(RenderListIndex),
+    commands: std.ArrayList(RenderListCommand),
+
     pub fn init(gpa: std.mem.Allocator) RenderList {
         return .{
             .gpa = gpa,
-            .sub_lists = std.ArrayList(RenderListSegment).init(gpa),
+            .vertices = std.ArrayList(RenderListVertex).init(gpa),
+            .indices = std.ArrayList(RenderListIndex).init(gpa),
+            .commands = std.ArrayList(RenderListCommand).init(gpa),
         };
     }
     pub fn deinit(self: *RenderList) void {
-        for (self.sub_lists.items) |*item| item.deinit();
-        self.sub_lists.deinit();
+        self.vertices.deinit();
+        self.indices.deinit();
+        self.commands.deinit();
     }
     pub fn clear(self: *RenderList) void {
-        for (self.sub_lists.items) |*item| item.deinit();
-        self.sub_lists.clearRetainingCapacity();
+        self.vertices.clearRetainingCapacity();
+        self.indices.clearRetainingCapacity();
+        self.commands.clearRetainingCapacity();
     }
 
-    fn getSub(self: *RenderList, image: ?RenderListImage, min_remaining_vertices: usize) *RenderListSegment {
-        // empty case: create new sublist
-        if (self.sub_lists.items.len == 0) {
-            return self.addSub(image);
+    fn getCmd(self: *RenderList, image: ?RenderListImage, min_remaining_vertices: usize) *RenderListCommand {
+        // empty case: create a new command
+        if (self.commands.items.len == 0) {
+            return self.addCmd(image);
         }
-        const last = &self.sub_lists.items[self.sub_lists.items.len - 1];
+        const last = &self.commands.items[self.commands.items.len - 1];
         // last item does not have enough remaining indices to hold new vertices case: create new sublist
-        if (last.vertices.items.len + min_remaining_vertices > std.math.maxInt(RenderListIndex)) {
-            return self.addSub(image);
+        if (last.vertex_count + min_remaining_vertices > std.math.maxInt(RenderListIndex)) {
+            return self.addCmd(image);
         }
         // last item does not have an image assigned yet: assign image
         if (last.image == null) {
@@ -52,25 +70,34 @@ pub const RenderList = struct {
         }
         // last item has an image assigned, but it is not the right image: create new sublist
         if (image != null and last.image.? != image.?) {
-            return self.addSub(image);
+            return self.addCmd(image);
         }
         // last image has an image assigned and it is the right image
         return last;
     }
-    fn addSub(self: *RenderList, image: ?RenderListImage) *RenderListSegment {
-        self.sub_lists.append(RenderListSegment.init(self.gpa, image)) catch @panic("oom");
-        return &self.sub_lists.items[self.sub_lists.items.len - 1];
+    fn addCmd(self: *RenderList, image: ?RenderListImage) *RenderListCommand {
+        self.commands.append(.{
+            .vertex_count = 0,
+            .index_count = 0,
+            .first_index = @intCast(self.indices.items.len),
+            .base_vertex = @intCast(self.vertices.items.len), // not sure if this usage is right?
+            .image = image,
+        }) catch @panic("oom");
+        return &self.commands.items[self.commands.items.len - 1];
     }
-    fn addVertices(self: *RenderList, image: ?RenderListImage, vertices: []const RenderListVertex, indices: []const RenderListIndex) void {
-        var sub = self.getSub(image, vertices.len);
 
-        const prev_vertices_len: u16 = @intCast(sub.vertices.items.len); // vertices len can never be larger than (maxInt(u16) - vertices.len)
-        sub.vertices.appendSlice(vertices) catch @panic("oom");
-        sub.indices.ensureUnusedCapacity(indices.len) catch @panic("oom");
+    fn addVertices(self: *RenderList, image: ?RenderListImage, vertices: []const RenderListVertex, indices: []const RenderListIndex) void {
+        var cmd = self.getCmd(image, vertices.len);
+
+        const prev_vertices_len: u16 = @intCast(cmd.vertex_count); // vertices len can never be larger than (maxInt(u16) - vertices.len)
+        self.vertices.appendSlice(vertices) catch @panic("oom");
+        self.indices.ensureUnusedCapacity(indices.len) catch @panic("oom");
         for (indices) |index| {
             std.debug.assert(index < vertices.len);
-            sub.indices.appendAssumeCapacity(prev_vertices_len + index);
+            self.indices.appendAssumeCapacity(prev_vertices_len + index);
         }
+        cmd.vertex_count += vertices.len;
+        cmd.index_count += @intCast(indices.len);
     }
 
     pub fn addRect(self: *RenderList, pos: @Vector(2, f32), size: @Vector(2, f32), opts: struct {
@@ -99,22 +126,5 @@ pub const RenderList = struct {
             0, 1, 3,
             0, 3, 2,
         });
-    }
-};
-pub const RenderListSegment = struct {
-    vertices: std.ArrayList(RenderListVertex),
-    indices: std.ArrayList(RenderListIndex),
-    image: ?RenderListImage,
-
-    pub fn init(gpa: std.mem.Allocator, image: ?RenderListImage) RenderListSegment {
-        return .{
-            .vertices = std.ArrayList(RenderListVertex).init(gpa),
-            .indices = std.ArrayList(RenderListIndex).init(gpa),
-            .image = image,
-        };
-    }
-    pub fn deinit(self: *RenderListSegment) void {
-        self.vertices.deinit();
-        self.indices.deinit();
     }
 };
