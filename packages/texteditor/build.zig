e939bbac971fa94f3313494938620793c583cc39
pfg <pfg@pfg.pw>
2024-09-09 14:07:14 -0400

    wip: texteditor module with zigglyph for graphemes

--- packages/texteditor/build.zig
+++ packages/texteditor/build.zig
@@ -4,20 +4,24 @@ pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const fmt_step = b.addFmt(.{ .paths = &.{ "src", "build.zig" } });
+    const fmt_step = b.addFmt(.{ .paths = &.{ "src", "build.zig", "build.zig.zon" } });
     b.getInstallStep().dependOn(&fmt_step.step);
 
-    _ = b.addModule("blocks", .{
+    const zg_dep = b.dependency("zg", .{});
+
+    const blocks_mod = b.addModule("texteditor", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
+    blocks_mod.addImport("zg_grapheme", zg_dep.module("grapheme"));
 
     const block_test = b.addTest(.{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
+    block_test.root_module.addImport("zg_grapheme", zg_dep.module("grapheme"));
 
     b.installArtifact(block_test);
     const run_block_tests = b.addRunArtifact(block_test);
