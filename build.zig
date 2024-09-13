0071fdeff55bcec14ad8ba1b2a4f6c39386251ac
pfg <pfg@pfg.pw>
2024-09-13 17:36:04 -0400

    add a build.zig to project root

---
+++ build.zig
@@ -0,0 +1,10 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    _ = b;
+
+    // TODO:
+    // running `install` runs all install steps of all packages
+    // support `test` to run all tests
+    // support `run-blockeditor` to run blockeditor
+}
