2dad38279fc4ad9ca0b16537dad4be46f9d60b87
pfg <pfg@pfg.pw>
2024-09-17 07:23:17 -0400

    TODO texcache

---
+++ packages/beui/src/texcache.zig
@@ -0,0 +1,12 @@
+const std = @import("std");
+
+// pub fn put(type: struct {})
+
+// fn hashAnytype(value: anytype, result: *u64) void {
+//     if (@hasDecl(@TypeOf(value), "hash")) value.hash();
+
+//     // std.AutoArrayHashMap(comptime K: type, comptime V: type)
+// }
+
+// usage: put(anytype)
+// it will hash based on typeid and contents
