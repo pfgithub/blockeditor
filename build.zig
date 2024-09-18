a984a1b6b5f1cfb983f38e36c8dbd5e28628ef8b
pfg <pfg@pfg.pw>
2024-09-18 15:36:34 -0500

    usocket tcp server example

--- build.zig
+++ build.zig
@@ -23,8 +23,10 @@ pub fn build(b: *std.Build) void {
     const loadimage_dep = b.dependency("loadimage", .{ .target = target, .optimize = optimize });
     const texteditor_dep = b.dependency("texteditor", .{ .target = target, .optimize = optimize });
     const unicode_segmentation_dep = b.dependency("unicode_segmentation", .{ .target = target, .optimize = optimize });
+    const usockets_dep = b.dependency("usockets", .{ .target = target, .optimize = optimize });
 
     b.installArtifact(blockeditor_dep.artifact("blockeditor"));
+    b.installArtifact(usockets_dep.artifact("server"));
 
     const test_step = b.step("test", "Test");
     test_step.dependOn(b.getInstallStep());
@@ -33,10 +35,17 @@ pub fn build(b: *std.Build) void {
     test_step.dependOn(&b.addRunArtifact(loadimage_dep.artifact("test")).step);
     test_step.dependOn(&b.addRunArtifact(texteditor_dep.artifact("test")).step);
     test_step.dependOn(&b.addRunArtifact(unicode_segmentation_dep.artifact("test")).step);
+    test_step.dependOn(&b.addRunArtifact(usockets_dep.artifact("test")).step);
 
     const run_blockeditor = b.addRunArtifact(blockeditor_dep.artifact("blockeditor"));
     if (b.args) |args| run_blockeditor.addArgs(args);
     const run_blockeditor_step = b.step("run", "Run blockeditor");
     run_blockeditor_step.dependOn(b.getInstallStep());
     run_blockeditor_step.dependOn(&run_blockeditor.step);
+
+    const run_server = b.addRunArtifact(usockets_dep.artifact("server"));
+    if (b.args) |args| run_server.addArgs(args);
+    const run_server_step = b.step("server", "Run server");
+    run_server_step.dependOn(b.getInstallStep());
+    run_server_step.dependOn(&run_server.step);
 }
