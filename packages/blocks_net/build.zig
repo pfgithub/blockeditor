28ba4ee4c559f271489a05eab00a822a7d72e993
pfg <pfg@pfg.pw>
2024-09-21 21:30:02 -0500

    wsnc

--- packages/blocks_net/build.zig
+++ packages/blocks_net/build.zig
@@ -57,4 +57,18 @@ pub fn build(b: *std.Build) void {
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(b.getInstallStep());
     test_step.dependOn(&run_tests.step);
+
+    const client_exe = b.addExecutable(.{
+        .name = "wsnc",
+        .root_source_file = b.path("src/wsnc.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    client_exe.root_module.addImport("websocket", websocket_zig_dep.module("websocket"));
+
+    b.installArtifact(client_exe);
+    const run_client = b.addRunArtifact(client_exe);
+    run_client.step.dependOn(b.getInstallStep());
+    const run_client_step = b.step("wsnc", "Run wsnc");
+    run_client_step.dependOn(&run_client.step);
 }
---
+++ packages/blocks_net/src/wsnc.zig
@@ -0,0 +1,54 @@
+const std = @import("std");
+const ws = @import("websocket");
+
+const App = struct { client: ws.Client };
+
+pub fn main() !void {
+    var gpa_backing = std.heap.GeneralPurposeAllocator(.{}){};
+    defer std.debug.assert(gpa_backing.deinit() == .ok);
+    const gpa = gpa_backing.allocator();
+
+    var app: App = .{
+        .client = try ws.Client.init(gpa, .{
+            .port = 9224,
+            .host = "localhost",
+        }),
+    };
+    defer app.client.deinit();
+
+    try app.client.handshake("/ws", .{
+        .timeout_ms = 10_000,
+        .headers = "Host: localhost:9224", // separate multiple headers with \r\n
+    });
+
+    const recv_thread = try std.Thread.spawn(.{}, recvThread, .{&app});
+    defer recv_thread.join();
+
+    std.log.info("wsnc. enter to exit", .{});
+
+    // wait for enter key to be pressed
+    while (true) {
+        const msg = try std.io.getStdIn().reader().readUntilDelimiterAlloc(gpa, '\n', std.math.maxInt(usize));
+        defer gpa.free(msg);
+        if (std.mem.eql(u8, msg, "\n") or std.mem.eql(u8, msg, "\r\n") or std.mem.eql(u8, msg, "\r")) {
+            // exit
+            break;
+        }
+        std.log.info("sending \"{}\"...", .{std.zig.fmtEscapes(msg)});
+        try app.client.writeBin(msg);
+        std.log.info("-> sent", .{});
+    }
+
+    std.log.info("closing...", .{});
+    try app.client.close(.{});
+    std.log.info("-> closed", .{});
+}
+
+fn recvThread(self: *App) void {
+    while (true) {
+        const msg = (self.client.read() catch return) orelse unreachable;
+        defer self.client.done(msg);
+
+        std.log.info("received message: \"{}\"", .{std.zig.fmtEscapes(msg.data)});
+    }
+}
