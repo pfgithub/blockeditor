5eacdfe089d15fd0fffe8c9b5cf02ed8a3dcb6b2
pfg <pfg@pfg.pw>
2024-09-22 18:13:17 -0500

    targetScrollApi

--- packages/beui/src/beui_experiment.zig
+++ packages/beui/src/beui_experiment.zig
@@ -39,7 +39,7 @@ pub fn runExperiment(beui: *Beui, w: u32, h: u32) void {
     var layout_value: BeuiLayout = .{
         .beui = beui,
         .insert_cursor = .{ 0, 0 },
-        .size = .{ @intCast(w), @intCast(h) },
+        .size = .{ w, h },
         .flow_direction = .y,
     };
     const layout = &layout_value;
@@ -48,3 +48,61 @@ pub fn runExperiment(beui: *Beui, w: u32, h: u32) void {
     rect(layout, 50, .fromHexRgb(0x00FF00));
     rect(layout, 25, .fromHexRgb(0x0000FF));
 }
+
+pub const Scroller = struct {
+    // without being able to shift items after they are rendered, how do we measure the size of a child?
+    // either we have to be able to shift items, or we have to take two frames. frame one render the
+    // child but disable interaction on it and don't let it actually draw anything. frame two now we know
+    // its height, draw it like normal.
+    pub const State = struct {
+        offset: f32,
+        target_id: ID,
+        target_data: ?u128,
+    };
+};
+const ID = enum(u128) {};
+
+fn targetScrollApi(layout: *BeuiLayout, scroll_state: *Scroller.State) void {
+    const scroller = layout.beginScroll(scroll_state);
+    defer scroller.end();
+
+    if (scroller.child(@src())) rect(layout, 25, .fromHexRgb(0xFF0000));
+    if (scroller.child(@src())) rect(layout, 50, .fromHexRgb(0x00FF00));
+    if (scroller.child(@src())) rect(layout, 25, .fromHexRgb(0x0000FF));
+
+    // SizeOf(SampleVirtual) must be less than say 16 bytes and alignment must be 16 or less
+    while (scroller.virtual(@src(), SampleVirtual, {})) |value| {
+        const height: i64 = switch (value.v % 4) {
+            0 => 100,
+            1 => 80,
+            2 => 60,
+            3 => 30,
+            else => unreachable,
+        };
+        switch (value.v % 3) {
+            0 => rect(layout, height, .fromHexRgb(0xFFFF00)),
+            1 => rect(layout, height, .fromHexRgb(0x00FFFF)),
+            2 => rect(layout, height, .fromHexRgb(0xFF00FF)),
+            else => unreachable,
+        }
+    }
+}
+
+const SampleVirtual = struct {
+    v: u32,
+
+    pub fn first(_: void) SampleVirtual {
+        return .{ .v = 0 };
+    }
+    pub fn last(_: void) SampleVirtual {
+        return .{ .v = 100 };
+    }
+    pub fn prev(_: void, v: SampleVirtual) ?SampleVirtual {
+        if (v.v > 0) return .{ .v = v.v - 1 };
+        return null;
+    }
+    pub fn next(_: void, v: SampleVirtual) ?SampleVirtual {
+        if (v.v < 100) return .{ .v = v.v + 1 };
+        return null;
+    }
+};
