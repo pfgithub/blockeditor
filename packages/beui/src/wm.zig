014d596aa439cc2273b7b534768865d4fa037e55
pfg <pfg@pfg.pw>
2024-10-26 18:41:55 -0500

    some tests to fill out

--- packages/beui/src/wm.zig
+++ packages/beui/src/wm.zig
@@ -9,7 +9,9 @@ const FloatingContainer = struct {
 const InnerContainer = struct {
     parent: ?*InnerContainer,
     value: union(enum) {
-        final: B2.ID,
+        final: struct {
+            id: B2.ID,
+        },
         tabs: struct {
             active_tab: usize,
             children: std.ArrayList(*InnerContainer),
@@ -18,8 +20,51 @@ const InnerContainer = struct {
             direction: enum { x, y },
             items: std.ArrayList(SplitContainerInfo),
         },
-        floating: struct {},
     },
+
+    pub fn validate(node: *InnerContainer, parent: ?*InnerContainer) void {
+        std.debug.assert(node.parent == parent);
+        switch (node.value) {
+            .tabs => |*tcont| {
+                std.debug.assert(tcont.active_tab < tcont.children.items.len);
+                for (tcont.children.items) |ch| validate(ch, node);
+            },
+            .split => |*split| {
+                switch (split.direction) {
+                    .x => std.debug.assert(parent == null or (parent.?.value == .split and parent.?.value.split.direction != .x)),
+                    .y => std.debug.assert(parent == null or (parent.?.value == .split and parent.?.value.split.direction != .y)),
+                }
+                for (split.items.items, 0..) |ch, i| {
+                    if (i == 0) std.debug.assert(ch.start_px == 0);
+                    validate(ch.node, node);
+                }
+            },
+            .final => {},
+        }
+    }
+    pub fn format(self: *const InnerContainer, comptime _: []const u8, _: std.fmt.FormatOptions, writer: std.io.AnyWriter) !void {
+        try writer.writeAll("[");
+        switch (self.value) {
+            .final => |id| {
+                try writer.print("{d}", .{id.hash()});
+            },
+            .tabs => |t| {
+                try writer.print("T", .{});
+                for (t.children.items, 0..) |c, i| {
+                    if (i == 0) try writer.print("|", .{});
+                    try writer.print(" {}", .{c});
+                }
+            },
+            .split => |s| {
+                try writer.print("S", .{});
+                for (s.items.items, 0..) |c, i| {
+                    if (i == 0) try writer.print("|", .{});
+                    try writer.print(" {d},{}", .{ c.start_px, c.node });
+                }
+            },
+        }
+        try writer.writeAll("]");
+    }
 };
 const SplitContainerInfo = struct {
     start_px: f32, // first in the list is 0.
@@ -44,26 +89,8 @@ const WindowManager = struct {
         wm.id_to_inner_container_map.deinit();
     }
 
-    const ValidateParent = enum { split_x, split_y, other };
-    pub fn validate(node: *InnerContainer, parent: ValidateParent) void {
-        switch (node.value) {
-            .tabs => |*tcont| {
-                std.debug.assert(tcont.active_tab < tcont.children.items.len);
-                for (tcont.children.items) |ch| validate(ch, .other);
-            },
-            .split => |*split| {
-                switch (split.direction) {
-                    .x => std.debug.assert(parent != .split_x),
-                    .y => std.debug.assert(parent != .split_y),
-                }
-                const sv: ValidateParent = switch (split.direction) {
-                    .x => .split_x,
-                    .y => .split_y,
-                };
-                for (split.items.items) |ch| validate(ch.node, sv);
-            },
-            .final => {},
-        }
+    pub fn validate(self: *const WindowManager) void {
+        for (self.floating_containers.items) |fc| fc.child.validate(null);
     }
 
     fn getFloatingContainerForInnerContainer(self: *WindowManager, ic: *InnerContainer) ?*FloatingContainer {
@@ -122,13 +149,13 @@ const WindowManager = struct {
     /// editor would hold a reference to the text block being edited.
     /// also todo default layout stuff, ie "open this window to the right of this other window"
     pub fn getOrOpenWindow(self: *WindowManager, child: B2.ID) *InnerContainer {
-        if(self.id_to_inner_container_map.get(child)) |v| {
+        if (self.id_to_inner_container_map.get(child)) |v| {
             return v;
         }
         const res = self._addInnerContainer(.{
-                .parent = null,
-                .final = child,
-            });
+            .parent = null,
+            .final = child,
+        });
         self.floating_containers.append(.{
             .position = .{ 10, 10 },
             .size = .{ 100, 100 },
@@ -136,15 +163,63 @@ const WindowManager = struct {
         });
         return res;
     }
+
+    pub fn format(self: *const WindowManager, comptime _: []const u8, _: std.fmt.FormatOptions, writer: std.io.AnyWriter) !void {
+        self.validate();
+        for (self.floating_containers.items, 0..) |fc, i| {
+            if (i != 0) try writer.writeAll(" ");
+            try writer.print("{d},{d},{d},{d},[{}]", .{ fc.position[0], fc.position[1], fc.size[0], fc.size[1], fc.child });
+        }
+    }
 };
 
 test WindowManager {
+    var arena_backing = std.heap.ArenaAllocator.init(std.testing.allocator);
+    defer arena_backing.deinit();
+    const arena = arena_backing.allocator();
+
     var wm: WindowManager = .init(std.testing.allocator);
     defer wm.deinit();
 
-    // add a window
-    // const win = wm.getOrOpenWindow();
-    // _ = win;
+    try std.testing.expectEqualStrings("", try std.fmt.allocPrint(arena, "{}", .{wm}));
 
-    // find the window for an id
+    if (true) return error.SkipZigTest;
+
+    // add a window
+    const win1 = wm.getOrOpenWindow(1);
+    try std.testing.expectEqualStrings("10,10,100,100,[1]", try std.fmt.allocPrint(arena, "{}", .{wm}));
+
+    // add another window
+    const win2 = wm.getOrOpenWindow(2);
+    try std.testing.expectEqualStrings("10,10,100,100,[1] 10,10,100,100,[2]", try std.fmt.allocPrint(arena, "{}", .{wm}));
+
+    // bring to front
+    wm.bringToFrontWindow(win1);
+    try std.testing.expectEqualStrings("10,10,100,100,[2] 10,10,100,100,[1]", try std.fmt.allocPrint(arena, "{}", .{wm}));
+
+    // move window
+    wm.moveFloating(win2, .{ 20, 20 }, .{ true, true, true, true });
+    try std.testing.expectEqualStrings("30,30,100,100,[2] 10,10,100,100,[1]", try std.fmt.allocPrint(arena, "{}", .{wm}));
+
+    // dock to the right
+    wm.dropWindow(win2, win1, .right);
+    try std.testing.expectEqualStrings("10,10,100,100,[S| 0,[1] 50,[2]]", try std.fmt.allocPrint(arena, "{}", .{wm}));
+
+    // resize
+    wm.moveSplit(win2.parent, 1, -30);
+    try std.testing.expectEqualStrings("10,10,100,100,[S| 0,[1] 20,[2]]", try std.fmt.allocPrint(arena, "{}", .{wm}));
+
+    // pick up to move
+    wm.floatWindow(win2, .{ 50, 50 });
+    try std.testing.expectEqualStrings("10,10,100,100,[1] 50,50,100,100,[2]", try std.fmt.allocPrint(arena, "{}", .{wm}));
+
+    // tab it
+    wm.dropWindow(win2, win1, .tab);
+    try std.testing.expectEqualStrings("10,10,100,100,[T| [1] [2]]", try std.fmt.allocPrint(arena, "{}", .{wm}));
+
+    // reorder tabs
+    // (TODO? how to implement this?)
 }
+
+// it might be useful to try to implement this as a generic tree with no validation
+// - every state is valid as long as the tree invariants are kept
