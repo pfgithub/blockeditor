ffe2661f6e66dc2a9f788f6725568a0de71b2d9b
pfg <pfg@pfg.pw>
2024-10-26 00:50:10 -0500

    notes

--- packages/beui/src/wm.zig
+++ packages/beui/src/wm.zig
@@ -66,13 +66,46 @@ const WindowManager = struct {
         }
     }
 
-    fn moveFloating(id: *FloatingContainer, offset: @Vector(2, f32), anchors: [4]bool) void {
-        _ = id;
-        _ = offset;
-        _ = anchors;
+    fn getFloatingContainerForInnerContainer(self: *WindowManager, ic: *InnerContainer) ?*FloatingContainer {
+        var parentmost_window = ic;
+        while(parentmost_window.parent) |v| parentmost_window = v;
+        for(self.floating_containers.items) |c| {
+            if(c.child == parentmost_window) return c;
+        } else {
+            return null;
+        }
     }
-    fn moveSplit(id: *InnerContainer, offset: @Vector(2, f32)) void {
-        _ = id;
+
+    fn moveFloating(self: *WindowManager, ic: *InnerContainer, offset: @Vector(2, f32), anchors: [4]bool) void {
+        // go up to the parent floating container & move it
+        const floating_target = self.getFloatingContainerForInnerContainer(ic) orelse {
+            std.log.warn("moveFloating() called on window which no longer exists");
+            return;
+        };
+        var x1, var y1 = floating_target.position;
+        var x2, var y2 = floating_target.position + floating_target.size;
+
+        if(anchors[0]) x1 += offset[0];
+        if(anchors[1]) y1 += offset[1];
+        if(anchors[2]) x2 += offset[0];
+        if(anchors[3]) y2 += offset[1];
+
+        floating_target.position = .{x1, y1};
+        floating_target.size = .{x2 - x1, y2 - y1};
+
+        // min/max is not performed here; it is performed by the display instead
+        // ^ that doesn't quite make sense - once a drag is over, if the window has negative
+        //    size, we should fix its size so it doesn't appear to suddenly be broken.
+        //    - maybe when a drag is started, we should fix? that way if you make the window
+        //      smaller and a window is at the edge, it keeps its original position until you touch it?
+        //    - alternatively, we could store two positions: target position & actual position. when
+        //      starting a drag, set target position to actual position.
+    }
+    /// index is the index of the window after the split point
+    fn moveSplit(self: *WindowManager, ic: *InnerContainer, index: usize, offset: @Vector(2, f32)) void {
+        _ = self;
+        _ = ic;
+        _ = index;
         _ = offset;
     }
 };
@@ -80,4 +113,8 @@ const WindowManager = struct {
 test WindowManager {
     var wm: WindowManager = .init(std.testing.allocator);
     defer wm.deinit();
+
+    // add a window
+
+    // find the window for an id
 }
