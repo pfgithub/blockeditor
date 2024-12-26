9be11014034e37bf5f47d75f17122377fda15488
pfg <pfg@pfg.pw>
2024-12-26 15:35:28 -0500

    minigamer mouse events passed through now

--- packages/blockeditor/src/mini.zig
+++ packages/blockeditor/src/mini.zig
@@ -13,6 +13,7 @@ const State = struct {
     frame_out: *B2.ImageCache.Image,
     offsets_out: *OffsetsOut,
     bg_color_out: u32,
+    mouse: ?@Vector(2, i16) = null,
 
     pub fn init(self: *State, gpa: std.mem.Allocator) void {
         self.* = .{
@@ -34,6 +35,26 @@ const State = struct {
     }
 };
 
+const Data = struct {
+    state: *State,
+    center: @Vector(2, f32),
+    max_scale: f32,
+};
+pub fn render__onMouseEvent(data: *Data, b2: *B2.Beui2, mev: B2.MouseEvent) ?Beui.Cursor {
+    _ = b2;
+    if (mev.action == .down or mev.action == .move_while_down) {
+        const converted = (mev.pos.? - mev.capture_pos - data.center) / @as(@Vector(2, f32), @splat(data.max_scale));
+        data.state.mouse = .{
+            std.math.lossyCast(i16, converted[0]),
+            std.math.lossyCast(i16, converted[1]),
+        };
+    }
+    if (mev.action == .up) {
+        data.state.mouse = null;
+    }
+    return .arrow;
+}
+
 pub fn render(_: *const void, call_info: B2.StandardCallInfo, _: void) *B2.RepositionableDrawList {
     const ui = call_info.ui(@src());
     const rdl = ui.id.b2.draw();
@@ -46,15 +67,15 @@ pub fn render(_: *const void, call_info: B2.StandardCallInfo, _: void) *B2.Repos
     state.val.simulate(.{
         .time_ms = @bitCast(ui.id.b2.persistent.beui1.frame.frame_cfg.?.now_ms),
         .buttons = .{
-            .up = ui.id.b2.persistent.beui1.isKeyHeld(.up),
-            .left = ui.id.b2.persistent.beui1.isKeyHeld(.left),
-            .down = ui.id.b2.persistent.beui1.isKeyHeld(.down),
-            .right = ui.id.b2.persistent.beui1.isKeyHeld(.right),
-            .interact = ui.id.b2.persistent.beui1.isKeyHeld(.z),
-            .jump = ui.id.b2.persistent.beui1.isKeyHeld(.x),
-            .menu = ui.id.b2.persistent.beui1.isKeyHeld(.c),
+            .up = ui.id.b2.persistent.beui1.isKeyHeld(.up) or ui.id.b2.persistent.beui1.isKeyHeld(.w),
+            .left = ui.id.b2.persistent.beui1.isKeyHeld(.left) or ui.id.b2.persistent.beui1.isKeyHeld(.a),
+            .down = ui.id.b2.persistent.beui1.isKeyHeld(.down) or ui.id.b2.persistent.beui1.isKeyHeld(.s),
+            .right = ui.id.b2.persistent.beui1.isKeyHeld(.right) or ui.id.b2.persistent.beui1.isKeyHeld(.d),
+            .interact = ui.id.b2.persistent.beui1.isKeyHeld(.z) or ui.id.b2.persistent.beui1.isKeyHeld(.enter),
+            .jump = ui.id.b2.persistent.beui1.isKeyHeld(.x) or ui.id.b2.persistent.beui1.isKeyHeld(.space),
+            .menu = ui.id.b2.persistent.beui1.isKeyHeld(.c) or ui.id.b2.persistent.beui1.isKeyHeld(.escape),
         },
-        .mouse = null, // TODO
+        .mouse = state.mouse, // TODO
     }, .{
         .frame = std.mem.bytesAsSlice(u32, state.frame_out.mutate())[0 .. emu.constants.EMU_SCREEN_DATA_SIZE_U32 * emu.constants.EMU_SCREEN_NLAYERS],
         .layer_offsets = state.offsets_out,
@@ -97,6 +118,17 @@ pub fn render(_: *const void, call_info: B2.StandardCallInfo, _: void) *B2.Repos
         .size = size,
         .tint = .fromHexArgb(state.bg_color_out),
         .rounding = .{ .corners = .all, .radius = 6.0 },
+    });
+
+    const data = ui.id.b2.frame.arena.create(Data) catch @panic("oom");
+    data.* = .{
+        .state = state,
+        .center = center,
+        .max_scale = max_scale,
+    };
+
+    rdl.addMouseEventCapture2(ui.id.sub(@src()), .{ 0, 0 }, size, .{
+        .onMouseEvent = .from(data, render__onMouseEvent),
     });
 
     return rdl;
