617493afc6fb871dc70a07eba7a16e1f4e3bfb5b
pfg <pfg@pfg.pw>
2025-02-01 19:25:23 -0800

    .

--- packages/rvemu/tests/print_f32.zig
+++ packages/rvemu/tests/print_f32.zig
@@ -6,11 +6,18 @@ const lib = @import("lib/start.zig");
 
 pub const panic = lib.panic;
 export fn _start() noreturn {
-    var buf: [10]u8 = undefined;
+    var buf: [30]u8 = undefined;
     var val: f32 = 25.0;
     _ = &val;
-    const slice = std.fmt.bufPrint(&buf, "{d}", .{val}) catch "[ERR]";
+    var slice = std.fmt.bufPrint(&buf, "Print 25.0 ? {d}", .{val}) catch "[ERR]";
     _ = lib.syscall2(lib.Sys.print, @intFromPtr(slice.ptr), slice.len);
+
+    var val2: f32 = 10.0;
+    val = 1.0;
+    _ = &val2;
+    slice = std.fmt.bufPrint(&buf, "1.0 < 10.0 ? {}", .{val < val2}) catch "[ERR]";
+    _ = lib.syscall2(lib.Sys.print, @intFromPtr(slice.ptr), slice.len);
+
     _ = lib.syscall1(lib.Sys.exit, 0);
     unreachable;
 }
