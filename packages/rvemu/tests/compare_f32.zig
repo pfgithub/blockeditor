9172f11d23aa03b269b311b80a5fdf0e6ab96817
pfg <pfg@pfg.pw>
2025-02-02 11:56:58 -0800

    fix compare_f32 test

--- packages/rvemu/tests/compare_f32.zig
+++ packages/rvemu/tests/compare_f32.zig
@@ -6,7 +6,7 @@ export fn _start() noreturn {
     var buf: [30]u8 = undefined;
     const val1 = lib.forceRuntime(10.0);
     const val2 = lib.forceRuntime(1.0);
-    const slice = std.fmt.bufPrint(&buf, "1.0 < 10.0 ? {}", .{val1 < val2}) catch "[ERR]";
+    const slice = std.fmt.bufPrint(&buf, "1.0 < 10.0 ? {}", .{val2 < val1}) catch "[ERR]";
     _ = lib.syscall2(lib.Sys.print, @intFromPtr(slice.ptr), slice.len);
 
     _ = lib.syscall1(lib.Sys.exit, 0);
