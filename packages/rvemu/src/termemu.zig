c0de6e9148d120749cd9002e073ee715446d676e
pfg <pfg@pfg.pw>
2025-03-07 03:37:12 -0800

    to vtable

--- packages/rvemu/src/termemu.zig
+++ packages/rvemu/src/termemu.zig
@@ -1,3 +1,44 @@
+const Sys = extern struct {
+    data: *SysData,
+    vtable: *const SysVtable,
+};
+const SysData = opaque {};
+const SysVtable = struct {
+    waitEvent: *const fn (sys: *SysData) Event,
+    log: ?LogVtable,
+};
+const LogVtable = struct {
+    event_stdin: u64,
+    event_stdout: u64,
+    event_resize: u64,
+
+    group_log: *const fn (sys: *SysData, group: LogGroup, rendered: Rendered) void,
+    group_begin: *const fn (sys: *SysData, group: LogGroup) LogGroup,
+    group_end: *const fn (sys: *SysData, group: LogGroup) void,
+    group_beginUpdate: *const fn (sys: *SysData, group: LogGroup) UpdateGroup,
+
+    /// .none is returned for non-TTY outputs
+    group_registerResizeListener: *const fn (sys: *SysData, group: LogGroup) StdoutResizeListener,
+    group_unregisterResizeListener: *const fn (sys: *SysData, listener: StdoutResizeListener) void,
+
+    resizeToken_getSize: *const fn (sys: *SysData, token: StdoutResizeToken) [2]u32,
+
+    updateGroup_update: *const fn (sys: *SysData, group: UpdateGroup, rendered: Rendered) void,
+    updateGroup_end: *const fn (sys: *SysData, group: UpdateGroup, rendered: Rendered, mode: enum(u32) { clear, log }) void,
+
+    /// returns .none if someone else owns the stdin and it cannot be acquired
+    /// if data is available to read, a 'readable' event will be sent immediately.
+    updateGroup_tryTakeStdin: *const fn (sys: *SysData) StdinHandle,
+
+    stdinHandle_updateConfig: *const fn (sys: *SysData, handle: StdinHandle, cfg: StdinCfg) void,
+    stdinHandle_read: *const fn (sys: *SysData, handle: StdinHandle, token: StdinReadToken, buf: Slice) usize,
+    stdinHandle_close: *const fn (sys: *SysData, handle: StdinHandle) void,
+
+    /// if this returns a length < msg.len, you must wait for a new StdoutEvent before
+    /// you can write again.
+    group_writeStdout: *const fn (sys: *SysData, group: LogGroup, msg: Slice) usize,
+};
+
 const Event = extern struct {
     id: u64,
     value: u64,
@@ -5,43 +46,10 @@ const Event = extern struct {
 const StdoutEvent = void;
 const StdinEvent = StdinReadToken;
 const ResizeEvent = void;
-extern fn waitEvent() Event;
 
-const LogGroup = enum(u64) {
-    _,
-
-    pub extern fn log(group: LogGroup, rendered: Rendered) void;
-    pub extern fn begin(group: LogGroup) LogGroup;
-    pub extern fn end(group: LogGroup) void;
-    pub extern fn beginUpdate(group: LogGroup) UpdateGroup;
-    /// [0, 0] is returned for non-TTY terminals
-    pub extern fn getSize(group: LogGroup) [2]u32;
-
-    /// if this returns a length < msg.len, you must wait for a new StdoutEvent before
-    /// you can write again.
-    pub extern fn writeStdout(group: LogGroup, msg: Slice) usize;
-};
-const UpdateGroup = enum(u64) {
-    none,
-    _,
-
-    pub extern fn update(group: UpdateGroup, rendered: Rendered) void;
-    pub extern fn end(group: UpdateGroup, rendered: Rendered, mode: enum { clear, log }) void;
-
-    // returns .none if someone else owns the stdin and it cannot be acquired
-    // if data is available to read, a 'readable' event will be sent immediately.
-    pub extern fn tryTakeStdin(group: UpdateGroup, cfg: StdinCfg) StdinHandle;
-};
-const StdinHandle = enum(u64) {
-    none,
-    _,
-
-    pub extern fn updateConfig(handle: StdinHandle, cfg: StdinCfg) void;
-    pub extern fn read(handle: StdinHandle, token: StdinReadToken, buf: Slice) usize;
-    /// after closing, any future operations on this handle are errors. any unread data is sent to the
-    /// next reader. (unless the reader is in raw mode - don't buffer in this case)
-    pub extern fn close(handle: StdinHandle) void;
-};
+const LogGroup = enum(u64) { _ };
+const UpdateGroup = enum(u64) { none, _ };
+const StdinHandle = enum(u64) { none, _ };
 const StdinCfg = extern struct {
     /// in line mode, echo is true. in raw, echo is false.
     mode: enum { line, raw },
@@ -50,9 +58,9 @@ const StdinCfg = extern struct {
     enable_mouse_scroll: bool,
     intercept_ctrl_c: bool,
 };
-const StdinReadToken = enum(u64) {
-    _,
-};
+const StdinReadToken = enum(u64) { _ };
+const StdoutResizeListener = enum(u64) { none, _ };
+const StdoutResizeToken = enum(u64) { _ };
 const Slice = extern struct {
     ptr: [*]u8,
     len: usize,
