a2c660df5c1c75ee93520d15f3499536a152876a
pfg <pfg@pfg.pw>
2025-04-15 12:46:59 -0700

    wip on zig_cluster

--- packages/unicode_segmentation_2/src/zig_cluster.zig
+++ packages/unicode_segmentation_2/src/zig_cluster.zig
@@ -15,15 +15,46 @@ const ReverseCodepointIterator = struct {
     }
 };
 
+const Rule = enum {
+    GB1,
+    GB2,
+    GB3,
+    GB4_5,
+    GB6,
+    GB7,
+    GB8,
+    GB9,
+    GB9a,
+    GB9b,
+    GB9c,
+    GB11,
+    GB12_13,
+    GB999,
+    fn shouldBreak(self: Rule) bool {
+        return switch (self) {
+            .GB1, .GB2 => true,
+            .GB3 => false,
+            .GB4_5 => true,
+            .GB6, .GB7, .GB8 => false,
+            .GB9, .GB9a, .GB9b, .GB9c => false,
+            .GB11, .GB12_13 => false,
+            .GB999 => true,
+        };
+    }
+};
+
 // [...] [left] [right] | <- iter pointer
 // https://unicode.org/reports/tr29/#GB1
 // we could have this return the match code. ie return .GB4 / return .GB11 / ...
 // and then unicode could have their tests show the expected match code
-fn hasBoundary(iter: *ReverseCodepointIterator, data: *const grapheme.GraphemeData) bool {
+fn hasBoundary(iter: *ReverseCodepointIterator, data: *const grapheme.GraphemeData) Rule {
     const extended = true;
 
-    const right = iter.peekRight() orelse return true;
-    const left = iter.left() orelse return true;
+    // GB2
+    const right = iter.peekRight() orelse return .GB2;
+
+    // GB1
+    const left = iter.left() orelse return .GB1;
 
     const left_gbp = data.gbp(left);
     const left_indic_prop = data.indic(left);
@@ -34,78 +65,107 @@ fn hasBoundary(iter: *ReverseCodepointIterator, data: *const grapheme.GraphemeDa
 
     // GB3
     if (left == '\r' and right == '\n') {
-        return false;
+        return .GB3;
     }
 
     // GB4, GB5
     if (left_gbp == .Control or right_gbp == .Control or left == '\r' or left == '\n' or right == '\r' or right == '\n') {
-        return true;
+        return .GB4_5;
     }
 
     // GB6
     if (left_gbp == .L and (right_gbp == .L or right_gbp == .V or right_gbp == .LV or right_gbp == .LVT)) {
-        return false;
+        return .GB6;
     }
 
     // GB7
     if ((left_gbp == .LV or left_gbp == .V) and (right_gbp == .V or right_gbp == .T)) {
-        return false;
+        return .GB7;
     }
 
     // GB8
     if ((left_gbp == .LVT or left_gbp == .T) and right_gbp == .T) {
-        return false;
+        return .GB8;
     }
 
     // GB9
     if (right_gbp == .Extend or right_gbp == .ZWJ) {
-        return false;
+        return .GB9;
     }
 
     if (extended) {
         // GB9a
-        if (right_gbp == .SpacingMark) return false;
+        if (right_gbp == .SpacingMark) return .GB9a;
         // GB9b
-        if (right_gbp == .Prepend) return false;
+        if (right_gbp == .Prepend) return .GB9b;
     }
 
-    if (extended) {
+    if (extended) extended: {
         // GB9c
         if (left_indic_prop == .Linker and right_indic_prop == .Consonant) {
-            // consonant [ extend linker ]* linker [extend <here>linker]*
-            // ? return false
-            // a bit complicated to implement
-            // @panic("TODO impl GB9c Indic_Conjunct_Break");
-            return false; // TODO
+            // samples:
+            // c ab ab ab ab b ab ab ab ab ab<
+            // c b<
+            // c ab b<
+            // c b ab<
+            var iter_dup = iter.*;
+            var allow_another_linker = true;
+            while (true) {
+                // just consumed .Linker
+                const cp = iter_dup.left() orelse break :extended;
+                const indic_prop = data.indic(cp);
+                switch (indic_prop) {
+                    .Linker => {
+                        // double-linker; only one of these is allowed
+                        if (!allow_another_linker) break :extended;
+                        allow_another_linker = false;
+                    },
+                    .Extend => {
+                        // consume next. it may be linker consonant
+                        // if it is extend, break :extended. if it is consonant, return false
+                        const nxt = iter_dup.left() orelse break :extended;
+                        const nxt_indic_prop = data.indic(nxt);
+                        switch (nxt_indic_prop) {
+                            .Linker => continue,
+                            .Consonant => return .GB9c,
+                            else => break :extended,
+                        }
+                    },
+                    .Consonant => return .GB9c,
+                    else => break :extended,
+                }
+            }
         }
     }
 
     // GB11
     if (left_gbp == .ZWJ and right_is_emoji) {
+        var iter_dup = iter.*;
         while (true) {
-            const codepoint = iter.left() orelse return true;
+            const codepoint = iter_dup.left() orelse break;
             const gbp = data.gbp(codepoint);
-            const is_emoji = data.isEmoji(codepoint);
             if (gbp == .Extend) continue;
-            if (is_emoji) return true;
+            const is_emoji = data.isEmoji(codepoint);
+            if (is_emoji) return .GB11;
             break;
         }
     }
 
     // GB12, GB13
     if (left_gbp == .Regional_Indicator and right_gbp == .Regional_Indicator) {
+        var iter_dup = iter.*;
         var count: usize = 0;
         while (true) {
-            const codepoint = iter.left() orelse break;
+            const codepoint = iter_dup.left() orelse break;
             const gbp = data.gbp(codepoint);
             if (gbp != .Regional_Indicator) break;
             count += 1;
         }
-        if (count % 2 == 1) return true;
+        if (count % 2 == 1) return .GB12_13;
     }
 
     // GB999
-    return true;
+    return .GB999;
 }
 
 test hasBoundary {
@@ -115,7 +175,7 @@ test hasBoundary {
     // TODO
 
     var iter: ReverseCodepointIterator = .{ .slice = &.{ 'a', 'b', 'c' }, .index = 2 };
-    try std.testing.expectEqual(true, hasBoundary(&iter, &gd));
+    try std.testing.expectEqual(.GB999, hasBoundary(&iter, &gd));
 
     var out_codepoints = std.ArrayList(u21).init(allocator);
     defer out_codepoints.deinit();
@@ -141,12 +201,10 @@ test hasBoundary {
 
         var iter2: ReverseCodepointIterator = .{ .slice = out_codepoints.items, .index = undefined };
         for (out_breaks.items, out_break_pos.items, 0..) |should_break, out_break_pos_val, i| {
-            if (i == 0) continue;
-            if (i == out_breaks.items.len) continue;
             iter2.index = i;
             const actual = hasBoundary(&iter2, &gd);
-            if (actual != should_break) {
-                std.log.err("grapheme_break_test:{d}:{d}: expected {}, got {}:\n{s}\n{}^", .{ line_no, out_break_pos_val, should_break, actual, ReplaceUnicode{ .slice = line_full }, Indenter{ .value = out_break_pos_val } });
+            if (actual.shouldBreak() != should_break) {
+                std.log.err("grapheme_break_test:{d}:{d}: got {s}({s}), expected {s}:\n{s}\n{}^", .{ line_no, out_break_pos_val, @tagName(actual), if (actual.shouldBreak()) "B" else "n", if (should_break) "B" else "n", ReplaceUnicode{ .slice = line_full }, Indenter{ .value = out_break_pos_val } });
             }
         }
     }
@@ -198,11 +256,11 @@ const GraphemeBreakTestTkz = struct {
                 const next = self.nextByte() orelse @panic("bad data");
                 if (next == 183) {
                     out_breaks.append(true) catch @panic("oom");
-                    out_break_pos.append(self.idx) catch @panic("oom");
+                    out_break_pos.append(self.idx -| 1) catch @panic("oom");
                     continue;
                 } else if (next == 0x97) {
                     out_breaks.append(false) catch @panic("oom");
-                    out_break_pos.append(self.idx) catch @panic("oom");
+                    out_break_pos.append(self.idx -| 1) catch @panic("oom");
                     continue;
                 } else std.debug.panic("bad byte in if: {x}", .{next});
             }
