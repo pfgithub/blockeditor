09ac53280068ce84eef483530dbbd37f1880ddac
pfg <pfg@pfg.pw>
2025-06-11 23:37:27 -0700

    generated namings

--- packages/cvl2/src/cvl2.ts
+++ packages/cvl2/src/cvl2.ts
@@ -1,219 +1,343 @@
-{
-class Src {
-  str;
-  idx;
-  line_indent_level;
-  lyn;
-  col;
-  fname="file";
-  constructor(str) {this.str = str;this.idx = 0;this.calcIndent();this.lyn=1;this.col=1}
-  peek() {
-    return this.str[this.idx] ?? "";
-  }
-  take() {
-    const res = this.peek();
-    this.idx += res.length;
-    if(res === "\n") {
-      this.lyn += 1;
-      this.col  = 1;
-      this.calcIndent();
-    }
-    this.col += res.length;
-    return res;
-  }
-  calcIndent() {
-    let sub = this.str.substring(this.idx);
-    let idnt = sub.match(/^ */)[0];
-    this.line_indent_level = idnt.length;
-  }
+function unreachable(): never {
+    throw new Error("unreachable");
 }
 
-const src = new Src(`abc [
+class Source {
+    public text: string;
+    public currentIndex: number;
+    public currentLine: number;
+    public currentCol: number;
+    public filename: string;
+    public currentLineIndentLevel: number;
+
+    constructor(text: string) {
+        this.text = text;
+        this.currentIndex = 0;
+        this.currentLine = 1;
+        this.currentCol = 1;
+        this.filename = "file";
+        this.calculateIndent();
+    }
+
+    public peek(): string {
+        return this.text[this.currentIndex] ?? "";
+    }
+
+    public take(): string {
+        const character = this.peek();
+        this.currentIndex += character.length;
+
+        if (character === "\n") {
+            this.currentLine += 1;
+            this.currentCol = 1;
+            this.calculateIndent();
+        } else {
+            this.currentCol += character.length;
+        }
+        return character;
+    }
+
+    private calculateIndent(): void {
+        const subString = this.text.substring(this.currentIndex);
+        const indentMatch = subString.match(/^ */);
+        this.currentLineIndentLevel = indentMatch ? indentMatch[0].length : 0;
+    }
+}
+
+interface TokenPosition {
+    idx: number;
+    lyn: number;
+    col: number;
+}
+
+interface IdentifierToken {
+    kind: "ident";
+    pos: TokenPosition;
+    str: string;
+}
+
+interface WhitespaceToken {
+    kind: "ws";
+    pos: TokenPosition;
+    nl: boolean;
+}
+
+interface OperatorToken {
+    kind: "op";
+    pos: TokenPosition;
+    op: string;
+}
+
+interface BlockToken {
+    kind: "block";
+    pos: TokenPosition;
+    start: string;
+    end: string;
+    items: SyntaxNode[];
+}
+
+interface BinaryExpressionToken {
+    kind: "binary";
+    pos: TokenPosition;
+    prec: number;
+    items: SyntaxNode[];
+}
+
+type SyntaxNode = IdentifierToken | WhitespaceToken | OperatorToken | BlockToken | BinaryExpressionToken;
+
+interface TokenizerStackItem {
+    idx: number;
+    lyn: number;
+    col: number;
+    char: string;
+    indent: number;
+    val: SyntaxNode[];
+    prec: number;
+    autoClose?: boolean;
+}
+
+interface TokenizationResult {
+    result: SyntaxNode[];
+    errors: (string | string[])[];
+}
+
+function tokenize(source: Source): TokenizationResult {
+    let currentSyntaxNodes: SyntaxNode[] = [];
+    const errors: (string | string[])[] = [];
+    const parseStack: TokenizerStackItem[] = [];
+
+    parseStack.push({ idx: 0, char: "", indent: -1, val: currentSyntaxNodes, prec: 0, lyn: 1, col: 1 });
+
+    while (source.peek()) {
+        const startIdx = source.currentIndex;
+        const startLine = source.currentLine;
+        const startCol = source.currentCol;
+        const currentChar = source.take();
+
+        const identifierRegex = /^[a-zA-Z0-9]$/;
+        const whitespaceRegex = /^\s$/;
+        const leftBrackets = ["(", "[", "{"];
+        const rightBrackets = [")", "]", "}"];
+
+        if (currentChar.match(identifierRegex)) {
+            while (source.peek().match(identifierRegex)) {
+                source.take();
+            }
+            currentSyntaxNodes.push({
+                kind: "ident",
+                pos: { idx: startIdx, lyn: startLine, col: startCol },
+                str: source.text.substring(startIdx, source.currentIndex),
+            });
+        } else if (currentChar.match(whitespaceRegex)) {
+            while (source.peek().match(whitespaceRegex)) {
+                source.take();
+            }
+            currentSyntaxNodes.push({
+                kind: "ws",
+                pos: { idx: startIdx, lyn: startLine, col: startCol },
+                nl: source.text.substring(startIdx, source.currentIndex).includes("\n"),
+            });
+        } else if (leftBrackets.includes(currentChar) || currentChar === ":") {
+            const newBlockItems: SyntaxNode[] = [];
+            currentSyntaxNodes.push({
+                kind: "block",
+                pos: { idx: startIdx, lyn: startLine, col: startCol },
+                start: currentChar,
+                end: currentChar === ":" ? "" : rightBrackets[leftBrackets.indexOf(currentChar)],
+                items: newBlockItems,
+            });
+            parseStack.push({
+                idx: startIdx,
+                lyn: startLine,
+                col: startCol,
+                char: currentChar,
+                indent: source.currentLineIndentLevel,
+                val: newBlockItems,
+                prec: currentChar === ":" ? 2 : 0,
+                autoClose: currentChar === ":",
+            });
+            currentSyntaxNodes = newBlockItems;
+        } else if (rightBrackets.includes(currentChar)) {
+            const correspondingLeftBracket = leftBrackets[rightBrackets.indexOf(currentChar)];
+            const currentIndent = source.currentLineIndentLevel;
+
+            while (parseStack.length > 1) {
+                const lastStackItem = parseStack.pop();
+                if (!lastStackItem) unreachable();
+
+                if (lastStackItem.char === correspondingLeftBracket && lastStackItem.indent === currentIndent) {
+                    currentSyntaxNodes = parseStack[parseStack.length - 1].val;
+                    break;
+                }
+
+                if (lastStackItem.indent < currentIndent) {
+                    errors.push(`${source.filename}:${startLine}:${startCol} error: extra close bracket`);
+                    parseStack.push(lastStackItem);
+                    break;
+                } else {
+                    if (!lastStackItem.autoClose) {
+                        errors.push([
+                            `${source.filename}:${lastStackItem.lyn}:${lastStackItem.col} error: open bracket missing close bracket`,
+                            `${source.filename}:${startLine}:${startCol} note: expected for '${lastStackItem.char}' indent '${lastStackItem.indent}', got '${currentChar}' indent '${currentIndent}'`,
+                        ]);
+                    }
+                    currentSyntaxNodes = parseStack[parseStack.length - 1].val;
+                }
+            }
+        } else if (currentChar === "," || currentChar === ";") {
+            const operatorPrecedence = 1;
+            let targetCommaBlock: TokenizerStackItem | undefined;
+
+            while (parseStack.length > 0) {
+                const lastStackItem = parseStack[parseStack.length - 1];
+
+                if (lastStackItem.prec === operatorPrecedence) {
+                    targetCommaBlock = lastStackItem;
+                    break;
+                } else if (lastStackItem.prec < operatorPrecedence) {
+                    targetCommaBlock = {
+                        idx: startIdx,
+                        lyn: startLine,
+                        col: startCol,
+                        char: currentChar,
+                        val: [...lastStackItem.val],
+                        indent: lastStackItem.indent,
+                        autoClose: true,
+                        prec: operatorPrecedence,
+                    };
+                    parseStack.push(targetCommaBlock);
+                    lastStackItem.val.splice(0, lastStackItem.val.length, {
+                        kind: "binary",
+                        pos: { idx: startIdx, lyn: startLine, col: startCol },
+                        prec: operatorPrecedence,
+                        items: targetCommaBlock.val,
+                    });
+                    break;
+                } else {
+                    if (!lastStackItem.autoClose) {
+                        errors.push([
+                            `${source.filename}:${startLine}:${startCol} auto-closing non auto-closeable item.`,
+                            `${source.filename}:${lastStackItem.lyn}:${lastStackItem.col} note: opened here`,
+                        ]);
+                    }
+                    parseStack.pop();
+                }
+            }
+
+            if (!targetCommaBlock) {
+                throw new Error("Unreachable: No target block found for comma/semicolon.");
+            }
+
+            currentSyntaxNodes = parseStack[parseStack.length - 1].val;
+            currentSyntaxNodes.push({
+                kind: "op",
+                pos: { idx: startIdx, lyn: startLine, col: startCol },
+                op: currentChar,
+            });
+        } else {
+            errors.push(`${source.filename}:${startLine}:${startCol} error: bad char ${JSON.stringify(currentChar)}`);
+        }
+    }
+
+    return { result: parseStack[0].val, errors };
+}
+
+interface RenderConfig {
+    indent: string;
+    style?: "s";
+}
+
+function renderEntityList(config: RenderConfig, entities: SyntaxNode[], level: number, isTopLevel: boolean): string {
+    let result = "";
+    let needsDeeperIndent = false;
+    let didInsertNewline = false;
+    let lastNewlineIndex = -1;
+
+    for (let i = 0; i < entities.length; i++) {
+        const entity = entities[i];
+        if (entity.kind === "ws" && entity.nl) {
+            lastNewlineIndex = i;
+        }
+    }
+
+    for (let i = 0; i < entities.length; i++) {
+        const entity = entities[i];
+        if (entity.kind === "ws") {
+            if (entity.nl && !didInsertNewline) {
+                needsDeeperIndent = !isTopLevel && i < lastNewlineIndex;
+                didInsertNewline = true;
+                result += "\n" + config.indent.repeat(level + (needsDeeperIndent ? 1 : 0));
+            } else {
+                result += " ";
+            }
+        } else {
+            didInsertNewline = false;
+            result += renderEntity(config, entity, level + (needsDeeperIndent ? 1 : 0), isTopLevel);
+        }
+    }
+    return result;
+}
+
+function renderEntity(config: RenderConfig, entity: SyntaxNode, level: number, isTopLevel: boolean): string {
+    if (config.style === "s") {
+        if (entity.kind === "block") {
+            return `(${JSON.stringify(entity.start + entity.end)} ` + renderEntityList(config, entity.items, level, false) + ")";
+        } else if (entity.kind === "binary") {
+            return "(binary " + renderEntityList(config, entity.items, level, isTopLevel) + ")";
+        } else if (entity.kind === "ws") {
+            throw new Error("Unreachable: Whitespace should be handled by renderEntityList.");
+        } else if (entity.kind === "ident") {
+            return "$" + entity.str;
+        } else if (entity.kind === "op") {
+            return JSON.stringify(entity.op);
+        } else {
+            return `(TODO $${(entity as {kind: string}).kind})`;
+        }
+    } else {
+        if (entity.kind === "block") {
+            return entity.start + renderEntityList(config, entity.items, level, false) + entity.end;
+        } else if (entity.kind === "binary") {
+            return renderEntityList(config, entity.items, level, isTopLevel);
+        } else if (entity.kind === "ws") {
+            throw new Error("Unreachable: Whitespace should be handled by renderEntityList.");
+        } else if (entity.kind === "ident") {
+            return entity.str;
+        } else if (entity.kind === "op") {
+            return entity.op;
+        } else {
+            return `%TODO<${(entity as {kind: string}).kind}>%`;
+        }
+    }
+}
+
+function renderTokenizedOutput(tokenizationResult: TokenizationResult, source: Source): string {
+    const formattedCode = renderEntityList({ indent: "  " }, tokenizationResult.result, 0, true);
+    const sExpression = renderEntityList({ indent: "  ", style: "s" }, tokenizationResult.result, 0, true);
+    const errorsJson = JSON.stringify(tokenizationResult.errors, null, 2);
+
+    return (
+        `// formatted\n${formattedCode}\n\n` +
+        `// s-expr:\n${sExpression}\n\n` +
+        `// errors:\n${errorsJson}`
+    );
+}
+const src = `abc [
     def [jkl]
     if (
-        amazing;
+            amazing;
     ] else {
-        wow!;
+            wow!;
     }
     demoFn(1, 2,
-    	3,
-    	4, 5, 6,
+        3,
+        4, 5, 6,
     7, 8)
     commaExample(1, 2, 3, 4)
     colonExample(a: 1, b: c: 2, 3)
-] ghi`);
-function tkz(src) {
-let current = [];
-let errors = [];
-const stack = [];
-stack.push({idx: 0, char: "", indent: -1, val: current,prec:0});
+] ghi`;
+if (import.meta.main) {
+    const sourceCode = new Source(src);
 
-while(src.peek()) {
-const start = src.idx;
-const start_lyn = src.lyn;
-const start_col = src.col;
-const char = src.take();
-
-const ident = /^[a-zA-Z0-9]$/;
-const ws = /^\s$/;
-const lb = [..."([{"];
-const rb = [...")]}"];
-if(char.match(ident)) {
-   while(src.peek().match(ident)) {
-      src.take();
-   }
-   current.push({kind:"ident",pos:{idx:start,lyn:start_lyn,col:start_col},str:src.str.substring(start, src.idx)});
-}else if(char.match(ws)) {
-   while(src.peek().match(ws)) {
-      src.take();
-   }
-   current.push({kind:"ws",pos:{idx:start,lyn:start_lyn,col:start_col},nl:src.str.substring(start, src.idx).includes("\n")});
-
-}else if(lb.includes(char)) {
-  let next = [];
-  current.push({kind: "block",pos:{idx:start,lyn:start_lyn,col:start_col}, start:char,end:rb[lb.indexOf(char)], items: next});
-  stack.push({idx: start,lyn:start_lyn,col:start_col, char, indent: src.line_indent_level, val: next, prec:0});
-  current = next;
-}else if(char === ":") {
-  let next = [];
-  current.push({kind: "block",pos:{idx:start,lyn:start_lyn,col:start_col}, start:char,end:"", items: next});
-  stack.push({idx: start,lyn:start_lyn,col:start_col, char, indent: src.line_indent_level, val: next, prec:2, autoClose: true});
-  current = next;
-}else if(rb.includes(char)) {
-  const b = lb[rb.indexOf(char)];
-  const indent = +src.line_indent_level;
-  while(stack.length > 1) {
-		const last = stack.pop();
-    if(last.char === b && last.indent === indent) {
-      // perfect
-      current = stack[stack.length - 1].val;
-      break;
-    }
-    if(last.indent < indent) {
-      errors.push(`${src.fname}:${start_lyn}:${start_col} error: extra close bracket`);
-      stack.push(last);
-      break;
-    }else{
-      if(!last.autoClose) errors.push([`${src.fname}:${last.lyn}:${last.col} error: open bracket missing close bracket`,`${src.fname}:${start_lyn}:${start_col} note: expected for '${last.char}' indent '${last.indent}', got '${char}' indent '${indent}'`]);
-      current = stack[stack.length - 1].val;
-    }
-  }
-}else if(char === "," || char === ";") {
-  const op_prec = 1;
-  // find spot with prec <= 1
-  let atComma;
-let  i = 0;
-  while(stack.length > 0) {
-i++;
-if(i > 1000) throw new Error("oops");
-    const last = stack[stack.length - 1];
-    if(last.prec === op_prec) {
-      atComma = last;
-      break;
-    }else if(last.prec < op_prec) {
-    	atComma = {
-        idx:start,
-        lyn:start_lyn,
-        col:start_col,
-        char: char,
-        val: [...last.val],
-        indent: last.indent,
-        autoClose: true,
-        prec:op_prec,
-      }
-      stack.push(atComma);
-      last.val.splice(0, last.val.length, {
-			    kind: "binary",
-          pos:{idx:start,lyn:start_lyn,col:start_col},
-          prec: op_prec,
-					items: atComma.val,
-      });
-      break;
-    }else{
-      if(!last.autoClose) errors.push([`${src.fname}:${start_lyn}:${start_col} auto-closing non autoclose-able item. ${src.frame}:${last.lyn}:${last.col} note: opened here`]);
-      stack.pop();
-    }
-  }
-  if(!atComma) throw new Error("unreachable");
-  // if(atComma.char !== char) errors.push(`${src.fname}:${start_lyn}:${start_col} mixed`);
-  current = stack[stack.length - 1].val;
-  current.push({
-    kind: "op",
-    pos:{idx:start,lyn:start_lyn,col:start_col},
-    op: char,
-  });
-  
-}else{
-
-errors.push(`${src.fname}:${start_lyn}:${start_col} error: bad char \"${JSON.stringify(char)}\"`);
-
-}
-
-}
-
-return {result:stack[0].val,errors};
-}
-
-  
-function renderEntityList(cfg, ents, level, toplevel) {
-let res = "";
-let di = false;
-let dn = false;
-let lastNewline = 0;
-for(let i = 0; i < ents.length; i++) {
-  const it = ents[i];
-	if(it.kind === "ws" && it.nl) lastNewline = i;
-}
-for(let i = 0; i < ents.length; i++) {
-  const it = ents[i];
-  if(it.kind === "ws") {
-    if(it.nl && !dn){
-      di = !toplevel && i < lastNewline;
-      dn = true;
-      res += "\n" + cfg.indent.repeat(level+di);
-    }else{
-			res += " ";
-    }
-  }else{
-    dn = false;
-  	res += renderEntity(cfg, it, level + di, toplevel);
-  }
-}
-return res;
-}
-function renderEntity(cfg, ent, level, toplevel) {
-if(cfg.style === "s") {
-  if(ent.kind === "block") {
-  return `(${JSON.stringify(ent.start + ent.end)} ` + renderEntityList(cfg, ent.items, level, false) + ")";
-  }else if(ent.kind === "binary") {
-  return "(binary " + renderEntityList(cfg, ent.items, level, toplevel) + ")";
-  }else if(ent.kind === "ws") {
-  throw new Error("unreachable");
-  }else if(ent.kind === "ident") {
-  return "$" + ent.str;
-  }else if(ent.kind === "op") {
-  return JSON.stringify(ent.op);
-  }else return "(TODO $"+ent.kind+")";
-}else{
-  if(ent.kind === "block") {
-  return ent.start + renderEntityList(cfg, ent.items, level, false) + ent.end;
-  }else if(ent.kind === "binary") {
-  return renderEntityList(cfg, ent.items, level, toplevel);
-  }else if(ent.kind === "ws") {
-  throw new Error("unreachable");
-  }else if(ent.kind === "ident") {
-  return ent.str;
-  }else if(ent.kind === "op") {
-  return ent.op;
-  }else return "%TODO<"+ent.kind+">%";
-}
-}
-
-
-function renderTkz(tkz, src) {
-
-return "// formatted\n" + renderEntityList({indent: "  "}, tkz.result, 0, true)+ "\n\n// s-expr:\n" + renderEntityList({indent: "  ",style:"s"}, tkz.result, 0, true) + "\n\n// errors:\n" + JSON.stringify(tkz.errors, null, 2);
-}
-
-console.log(renderTkz(tkz(src), src))
+    const tokenized = tokenize(sourceCode);
+    console.log(renderTokenizedOutput(tokenized, sourceCode));
 }
